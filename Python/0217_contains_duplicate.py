7ffee384382c78f3ec16378cab62c6f4fb2a0be1
Illia Polishchuk <iliya.polischuk22@gmail.com>
2025-06-11 08:30:47 -0400

    Solution to Contains Duplicate Pushed

---
+++ Python/0217_contains_duplicate.py
@@ -0,0 +1,12 @@
+# 0217 Contains Duplicate 
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+
+        hashset = set()
+
+        for n in nums:
+            if n in hashset:
+                return True
+            
+            hashset.add(n)
+        return False
