352c5f0bebaa76e53edef5bad0af5a034071a904
Illia Polishchuk <iliya.polischuk22@gmail.com>
2025-06-12 09:34:10 -0400

    Add Blind 75 Solution: 347. Top K Frequent Elements

---
+++ Python/0347_top_k_frequent_elements.py
@@ -0,0 +1,21 @@
+# 347. top k frequent elements
+# Time complexity: O(n)
+# Space complexity: O(n)
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]  
+
+        for n in nums:
+            count[n] = 1 + count.get(n,0)
+        for n, c in count.items():
+            freq[c].append(n)
+
+        res = []
+        for i in range(len(freq) -1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+            if len(res) == k:
+                return res
+        
