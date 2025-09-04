4a284f2040efd98307de7b0ad7c31c0f8048e391
Illia Polishchuk <iliya.polischuk22@gmail.com>
2025-09-04 18:25:02 -0400

    Add solution to SQL 50: 1141 User Activity For Past 30 Days 1

---
+++ SQL/1141_user_activity_for_past_30_days_1.sql
@@ -0,0 +1,5 @@
+-- User Activity for Past 30 Days I
+SELECT activity_date AS day, COUNT(DISTINCT(user_id)) AS active_users
+FROM Activity 
+WHERE activity_date <= "2019-07-27" AND DATEDIFF("2019-07-27", activity_date) < 30
+GROUP BY activity_date
