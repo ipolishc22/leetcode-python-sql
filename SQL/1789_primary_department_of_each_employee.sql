52b1ce8b51c2bed8d233098a1478270a226b7f89
Illia Polishchuk <iliya.polischuk22@gmail.com>
2025-10-03 13:35:02 -0400

    Add solution to SQL 50: 1789 Primary Department of Each Employee

---
+++ SQL/1789_primary_department_of_each_employee.sql
@@ -0,0 +1,9 @@
+-- 1789 Primary Department of Each Emploee
+SELECT DISTINCT employee_id, department_id
+FROM Employee 
+WHERE primary_flag = "Y"
+OR employee_id IN 
+ (SELECT employee_id 
+ FROM Employee 
+ GROUP BY employee_id
+ HAVING COUNT(department_id) = 1)
